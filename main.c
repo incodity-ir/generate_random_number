cfb1e80c29698e9b784bbd0f9bfac3f9b775610d
Ali Majidi <seyedalimajidi76@gmail.com>
2023-10-21 10:53:14 +0330

    helper.c definition

--- main.c
+++ main.c
@@ -16,20 +16,3 @@ int main()
 
     return 0;
 }
-
-void generate_random_numbers(int *buf,size_t count,int min,int max)
-{
-    for(int i=0;i<count;i++)
-    {
-        *(buf+i) =min + random() % (max-min+1);
-    }
-}
-
-
-void print_array(int *buf,size_t count)
-{
-    for(int i=0;i<count;i++)
-        printf("%d ", *(buf+i));
-
-    printf("\n");
-}
